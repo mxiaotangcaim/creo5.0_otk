38af9b2addb54e93ab563c265db50c6d56f67466
xiaotang <15651955906@163.com>
2020-01-02 13:39:09 +0800

    通用函数

--- otk_exe1/tools.h
+++ otk_exe1/tools.h
@@ -1,4 +1,14 @@
 #pragma once
 inline void LogMsg(xstring msg, xbool pop=true);
 void LogException(xthrowable_ptr x);
+void SplitString(const string&s, vector<string>&v, const string c);
 xstring GetFileNameWithVersion(pfcModel_ptr model);
+xstring GetFirstOption(wfcWSession_ptr sess, xstring cfgName);
+xbool ConfirmAction(wfcWSession_ptr sess, xstring msg);
+void ExtractModelFiles(pfcAssembly_ptr assembly, set<string>&modelFiles);
+template<typename T>
+xstring AnyToString(const T &data) {
+	stringstream ss;
+	ss << data;
+	return xstring(ss);
+}
